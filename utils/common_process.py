98af85d93c8f6d4a87ce43c27b1190dc0d6b361d
박정운 <duqrlwjddns1@naver.com>
2022-07-14 10:36:11 +0900

    Add. 나무위키 전처리 소스

--- utils/common_process.py
+++ utils/common_process.py
@@ -1,75 +1,100 @@
 import re
 
-class Preprocessor:
-    def __init__(self):
-        self.space = re.compile(' {2,}')
-        self.normal_text = re.compile('[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9,./<>?;:\'\"\[\]{}\\\|`~!@#$%^&*()\-_=+《》「」『』·ㆍ… ]')
 
-    def uniformize(self, line):
-        # change the special characters to normal charcters
+def uniformize(text: str) -> str:
+    """
+    특수문자를 일반문자로 변환
+    :param text: 텍스트
+    :return: 변환된 텍스트
+    """
+    # ㅁ
+    text = text.replace("＃", "#")
+    text = text.replace("＆", "&")
+    text = text.replace("＊", "*")
+    text = text.replace("＠", "@")
 
-        # ㅁ
-        line = line.replace("＃", "#")
-        line = line.replace("＆", "&")
-        line = line.replace("＊", "*")
-        line = line.replace("＠", "@")
+    # ㄴ
+    text = text.replace("＂", "\"")
+    text = text.replace("（", "(")
+    text = text.replace("）", ")")
+    text = text.replace("［", "[")
+    text = text.replace("］", "]")
+    text = text.replace("｛", "{")
+    text = text.replace("｝", "}")
+    text = text.replace("‘", "'")
+    text = text.replace("’", "'")
+    text = text.replace("“", "\"")
+    text = text.replace("”", "\"")
+    text = text.replace("〔", "[")
+    text = text.replace("〕", "]")
+    text = text.replace("〈", "<")
+    text = text.replace("〉", ">")
 
-        # ㄴ
-        line = line.replace("＂", "\"")
-        line = line.replace("（", "(")
-        line = line.replace("）", ")")
-        line = line.replace("［", "[")
-        line = line.replace("］", "]")
-        line = line.replace("｛", "{")
-        line = line.replace("｝", "}")
-        line = line.replace("‘", "'")
-        line = line.replace("’", "'")
-        line = line.replace("“", "\"")
-        line = line.replace("”", "\"")
-        line = line.replace("〔", "[")
-        line = line.replace("〕", "]")
-        line = line.replace("〈", "<")
-        line = line.replace("〉", ">")
+    # ㄹ
+    text = text.replace("％", "%")
 
-        # ㄹ
-        line = line.replace("％", "%")
+    # ㄱ
+    text = text.replace("　", " ")
+    text = text.replace("！", "!")
+    text = text.replace("＇", "'")
+    text = text.replace("，", ",")
+    text = text.replace("．", ".")
+    text = text.replace("／", "/")
+    text = text.replace("：", ":")
+    text = text.replace("；", ";")
+    text = text.replace("？", "?")
+    text = text.replace("、", ",")
+    text = text.replace("∼", "~")
+    text = text.replace("´", "'")
+    text = text.replace("～", "~")
 
-        # ㄱ
-        line = line.replace("　", " ")
-        line = line.replace("！", "!")
-        line = line.replace("＇", "'")
-        line = line.replace("，", ",")
-        line = line.replace("．", ".")
-        line = line.replace("／", "/")
-        line = line.replace("：", ":")
-        line = line.replace("；", ";")
-        line = line.replace("？", "?")
-        line = line.replace("、", ",")
-        line = line.replace("∼", "~")
-        line = line.replace("´", "'")
-        line = line.replace("～", "~")
+    # ㄷ
+    text = text.replace("＋", "+")
+    text = text.replace("－", "-")
+    text = text.replace("＜", "<")
+    text = text.replace("＝", "=")
+    text = text.replace("＞", ">")
 
-        # ㄷ
-        line = line.replace("＋", "+")
-        line = line.replace("－", "-")
-        line = line.replace("＜", "<")
-        line = line.replace("＝", "=")
-        line = line.replace("＞", ">")
+    # edit special characters
+    text = text.replace("&lt;", "<")
+    text = text.replace("&gt;", ">")
+    text = text.replace("&amp;", "&")
+    text = text.replace("&quot;", "\"")
 
-        # edit special characters
-        line = line.replace("&lt;", "<")
-        line = line.replace("&gt;", ">")
-        line = line.replace("&amp;", "&")
-        line = line.replace("&quot;", "\"")
+    return text
 
-        return line
 
-    def fill_space(self, line):
-        line = line.strip()
-        # prevent more than 2 spaces
-        line = self.space.sub(" ", line)
+def filter_text(text: str) -> str:
+    """
+    필요한 문자 이외의 것 제거 (영어 외 외국어 등)
+    :param text: 텍스트
+    :return: 불필요한 문자 제거 후 남은 텍스트
+    """
+    text = re.sub(r"[^가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9,./<>?;:\'\"\[\]{}\\\|`~!@#$%^&*()\-_=+《》「」『』·ㆍ… ]", "", text)
 
-        return line
+    return text
 
-    def pick_texts(self, line, not_used_text=None):
-        
+
+def normalize_space(text: str) -> str:
+    """
+    여러 공백(띄어쓰기, 개행 등)을 하나의 공백(띄어쓰기)으로 치환
+    :param text:
+    :return:
+    """
+    text = re.sub(r"\s{2,}", " ", text)  # 2개 이상 공백을 1개 공백으로 정규화
+    text = text.strip()
+
+    return text
+
+
+def filter_length(text: str, min_limit: int):
+    """
+    최소 어절 개수 필터링
+    :param text: 원문 텍스트
+    :param min_limit: 최소 어절 개수
+    :return: 필터링 결과
+    """
+    if len(text.split()) >= min_limit:
+        return text
+    else:
+        return None
