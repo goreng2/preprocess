4e82c1b3b7e844e1de31b954524c0b466e11f5db
JeongWoonPark <pjw1210@kpvoice.com>
2022-07-26 14:20:31 +0900

    Style. 파일 개수 변수명 수정

--- utils/shard.sh
+++ utils/shard.sh
@@ -1,9 +1,9 @@
 INPUT_FILE="kowiki-20220701.txt"
 OUTPUT_DIR="kowiki-20220701-shard2048"
 OUTPUT_FILE_PREFIX="kowiki_"
-SPLIT=2048
+NUM=2048
 
-split -d -a 4 -n r/${SPLIT} ${INPUT_FILE} ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX}
+split -d -a 4 -n r/${NUM} ${INPUT_FILE} ${OUTPUT_DIR}/${OUTPUT_FILE_PREFIX}
 # "-d": 숫자로 표현
 #        ex) corpus_aa, corpus_ab, ... ⇒ corpus_00, corpus_01, ...
 # "-a": 0을 넣어 숫자 개수 맞추기
