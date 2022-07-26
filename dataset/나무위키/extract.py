52176a1a5a8129b6edd760ed683006fc58f9411e
JeongWoonPark <pjw1210@kpvoice.com>
2022-07-26 11:38:13 +0900

    Update. 인자 받기 함수화, 불필요한 로그 출력 제거

--- dataset/나무위키/extract.py
+++ dataset/나무위키/extract.py
@@ -1,11 +1,8 @@
-import os
 from multiprocessing import Pool
-from namuwiki.extractor import extract_text
-from utils import util
-import argparse
+from utils import utils
 from time import time
 from typing import Dict, Any
-from pprint import pprint
+from namuwiki.extractor import extract_text
 
 
 def extract(document: Dict[str, Any]) -> str:
@@ -17,38 +14,21 @@ def extract(document: Dict[str, Any]) -> str:
     return extract_text(document['text'])
 
 
-def main(input_path: str, output_path: str, num_processor: int):
-    # 설정값 검토
-    assert os.path.isfile(input_path) and not os.path.isdir(output_path), "I/O 파일 경로 확인"
-    assert num_processor < os.cpu_count(), f"총 CPU 개수 초과: {num_processor} > {os.cpu_count()}"
-    assert num_processor < len(os.sched_getaffinity(0)), f"사용 가능한 CPU 개수 초과: {num_processor} > {len(os.sched_getaffinity(0))}"  # 윈도우에선 에러 발생: module 'os' has no attribute 'sched_getaffinity'
-
+def main(input: str, output: str, num_process: int):
     # 파일 로드
-    print(f"Load File... \"{input_path}\"")
-    docs = util.load_json(input_path)  # List[Dict]
-    print(f"Total {len(docs):,} Documents")
+    docs = utils.load_json(input)  # List[Dict[str, Any]]
 
     # 병렬처리
-    print("Start Extract!")
-    print(f"# of Processor: {num_processor}")
-    with Pool(processes=num_processor) as p:
+    with Pool(processes=num_process) as p:
         start = time()
         contents = p.map(extract, docs)
         end = time()
         print(f"Done! ... Consume {int(end - start)} second")
-    print(f"Total {len(contents):,} lines (- {len(docs) - len(contents):,} lines)")
 
     # 파일 저장
-    print(f"Save Result... \"{output_path}\"")
-    util.save(output_path, "\n".join(contents))
+    utils.save(output, "\n".join(contents))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="나무위키 본문 추출")
-    parser.add_argument("-i", "--input", type=str, required=True, help="원본 경로")
-    parser.add_argument("-o", "--output", type=str, required=True, help="추출된 텍스트 파일 저장 경로")
-    parser.add_argument("-n", "--num_process", type=int, required=True, help="사용할 병렬처리 CPU 코어 개수")
-    args = parser.parse_args()
-    pprint(vars(args))  # 설정값 확인
-
-    main(*vars(args).values())
+    args = utils.parse_args()
+    main(**args)
