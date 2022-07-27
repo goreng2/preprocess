8cd73e0e5b884f92196c1cb9097847c9e069a384
JeongWoonPark <pjw1210@kpvoice.com>
2022-07-27 17:52:41 +0900

    Style. 주석 편집

--- utils/utils.py
+++ utils/utils.py
@@ -14,10 +14,10 @@ def parse_args() -> Dict[str, Union[str, int]]:
         'num_process': int
     }
     """
-    parser = argparse.ArgumentParser(description="데이터 전처리 인자 설정")
-    parser.add_argument("-i", "--input", type=str, required=True, help="입력 파일 경로")
+    parser = argparse.ArgumentParser(description="데이터 추출/처리 인자 설정")
+    parser.add_argument("-i", "--input", type=str, required=True, help="입력 파일/폴더 경로")
     parser.add_argument("-o", "--output", type=str, required=True, help="출력 파일 경로")
-    parser.add_argument("-n", "--num_process", type=int, required=True, help="사용할 병렬처리 CPU 코어 개수")
+    parser.add_argument("-n", "--num_process", type=int, required=True, help="병렬처리 CPU 코어 개수")
     args = parser.parse_args()
 
     # 설정값 확인
