e3839043bfcd339763bb62c98d8bb00565b51f87
JeongWoonPark <pjw1210@kpvoice.com>
2022-11-18 10:04:45 +0900

    Style. 주석 수정

--- utils/make_vocab.py
+++ utils/make_vocab.py
@@ -4,14 +4,12 @@ from tokenizers.models import WordPiece
 from tokenizers.pre_tokenizers import Whitespace
 from tokenizers.processors import TemplateProcessing
 from tokenizers.trainers import WordPieceTrainer
-from tokenizers.normalizers import Lowercase, NFD, StripAccents
+from tokenizers.normalizers import Lowercase, StripAccents
 import argparse
 
 
 def main(corpus, size, output):
     tokenizer = Tokenizer(WordPiece(unk_token="[UNK]"))
-    # tokenizer.normalizer = normalizers.Sequence([NFD(), Lowercase(), StripAccents()])
-    # NFD()를 적용하면 한글 자소 분리됨
     tokenizer.normalizer = normalizers.Sequence([Lowercase(), StripAccents()])
     tokenizer.pre_tokenizer = Whitespace()
     tokenizer.post_processor = TemplateProcessing(
@@ -32,9 +30,9 @@ def main(corpus, size, output):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="WordPiece Tokenizer 만들기")
-    parser.add_argument("--corpus", nargs="+", type=str, required=True, help="말뭉치 데이터 경로")
+    parser.add_argument("--corpus", nargs="+", type=str, required=True, help="path/to/corpus.txt")
     parser.add_argument("--size", type=int, required=True, help="Vocab 사이즈")
-    parser.add_argument("--output", type=str, required=True, help="Tokenizer 저장 경로")
+    parser.add_argument("--output", type=str, default="vocab.json", help="path/to/vocab.json")
     args = parser.parse_args()
 
     main(**vars(args))
